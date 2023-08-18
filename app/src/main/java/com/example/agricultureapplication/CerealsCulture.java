d599c00e86f5b46d62e17eb718a3f759d2ff0a7a
PotatoHakers <s.herza@mail.ru>
2023-08-18 12:49:30 +0300

    Adapter + activity for cereals culture

--- app/src/main/java/com/example/agricultureapplication/CerealsCulture.java
+++ app/src/main/java/com/example/agricultureapplication/CerealsCulture.java
@@ -1,7 +1,7 @@
 package com.example.agricultureapplication;
 
 import android.os.Bundle;
-
+import android.widget.GridView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CerealsCulture extends AppCompatActivity {
@@ -10,6 +10,24 @@ public class CerealsCulture extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cereals_culture);
+
+        int[] imageIds = {
+                R.drawable.black, R.drawable.black, R.drawable.black,
+
+                R.drawable.black, R.drawable.black, R.drawable.black,
+
+                R.drawable.black, R.drawable.black, R.drawable.black,
+
+                R.drawable.black, R.drawable.black, R.drawable.black,
+
+                R.drawable.black, R.drawable.black, R.drawable.black,
+
+                // Здесь перечислите остальные ресурсы для изображений
+        };
+
+        GridView gridView = findViewById(R.id.gridView);
+        ListItemCerealsCulture adapter = new ListItemCerealsCulture(this, imageIds);
+        gridView.setAdapter(adapter);
     }
-    }
+}
 
---
+++ app/src/main/java/com/example/agricultureapplication/ListItemCerealsCulture.java
@@ -0,0 +1,47 @@
+package com.example.agricultureapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class ListItemCerealsCulture extends BaseAdapter {
+
+    private Context context;
+    private int[] imageIds;
+
+    public ListItemCerealsCulture(Context context, int[] imageIds) {
+        this.context = context;
+        this.imageIds = imageIds;
+    }
+
+    @Override
+    public int getCount() {
+        return imageIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item_cereals_culture, null);
+        }
+
+        ImageView imageView = convertView.findViewById(R.id.imageView);
+        imageView.setImageResource(imageIds[position]);
+
+        return convertView;
+    }
+}
